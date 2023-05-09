c69128bce87f69896170be4f89cc3e0bc5b31c7e
InSikHwang <hwanginsick@gmail.com>
2023-05-09 17:30:42 +0900

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
+import App from "./App";
 
 const darkTheme = {
   textColor: "whitesmoke",
@@ -15,18 +13,11 @@ const lightTheme = {
   backgroundColor: "whitesmoke",
 };
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
